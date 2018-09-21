070ee4a531a733eb856e5b4fd6f1a5632004fee4
Matias <mpierdominici@itba.edu.ar>
2018-09-21 19:48:09 -0300

    casi terminado

--- Ej4/superposicionCascParal.m
+++ Ej4/superposicionCascParal.m
@@ -1,24 +1,24 @@
-med=csvread('med.csv');
-sim=csvread('simMed.csv');
+med=csvread('3etapaParaleloAten.csv');
+sim=csvread('simAtenuacion.csv');
 
 maginitud=figure('Name','Bode magitud','NumberTitle','off');
 
-semilogx(med(:,1).*1000,20*log10(med(:,3)./med(:,2)),'-o','LineWidth',3);
-ylim([-7 7])
+semilogx(med(:,1),med(:,2),'LineWidth',3);
+
 hold on;
 
 semilogx(sim(:,1),sim(:,2),'LineWidth',2);
 formataxes('', 'Frecuencia (Hz)', 'Mangnitud (dB)', ... % titulo y ejes
-                    'Medici\''on','Simulaci\''on ')% legends
+                    'Paralelo','Cascada')% legends
 hold off;
 
 fas=figure('Name','Bode magitud','NumberTitle','off');
 
-semilogx(med(:,1).*1000,med(:,4),'-o','LineWidth',3);
+semilogx(med(:,1),(med(:,3)),'LineWidth',3);
 hold on;
 
 semilogx(sim(:,1),wrapTo360(sim(:,3)),'LineWidth',2);
 
 formataxes('', 'Frecuencia (Hz)','Fase ($\circ$)', ... % titulo y ejes
-                    'Medici\''on','Simulaci\''on ')% legends
+                    'Paralelo','Cascada')% legends
 hold off;
