d745b8fc82a246f5162b2cb720777ecb3898c56c
Matias <mpierdominici@itba.edu.ar>
2018-09-25 16:33:49 -0300

    se modificaron los todo, y el zzz que quedo dando vuelta, tmabien una medicion sospechoza

--- Ej4/superposicion.m
+++ Ej4/superposicion.m
@@ -7,17 +7,17 @@ semilogx(med(:,1).*1000,20*log10(med(:,3)./med(:,2)),'-o','LineWidth',3);
 ylim([-7 7])
 hold on;
 
-semilogx(sim(:,1),sim(:,2),'LineWidth',2);
+%semilogx(sim(:,1),sim(:,2),'LineWidth',2);
 formataxes('', 'Frecuencia (Hz)', 'Mangnitud (dB)', ... % titulo y ejes
                     'Medici\''on','Simulaci\''on ')% legends
 hold off;
 
 fas=figure('Name','Bode magitud','NumberTitle','off');
 
-semilogx(med(:,1).*1000,med(:,4),'-o','LineWidth',3);
+semilogx(med(:,1),med(:,4),'-o','LineWidth',3);
 hold on;
 
-semilogx(sim(:,1),wrapTo360(sim(:,3)),'LineWidth',2);
+semilogx(sim(:,1)./1000,wrapTo360(sim(:,3)),'LineWidth',2);
 
 formataxes('', 'Frecuencia (Hz)','Fase ($\circ$)', ... % titulo y ejes
                     'Medici\''on','Simulaci\''on ')% legends
